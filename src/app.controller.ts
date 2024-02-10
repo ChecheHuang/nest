c62373980ce76e295e04b03bd19eeb8462829bfc
Carl <qwerfcxzas1@gmail.com>
2024-02-10 17:25:36 +0800

    complete

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Inject } from '@nestjs/common'
 import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject('ABC') private readonly appService: AppService,
+    @Inject('Test') private readonly shop: string[],
+    @Inject('CCC') private readonly ccc: string,
+  ) {}
 
   @Get()
   getHello(): string {
+    console.log('this.ccc', this.ccc)
+    console.log('this.shop', this.shop)
     return this.appService.getHello()
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { AppService2 } from './app.service2'
 import { UsersModule } from './users/users.module'
 @Module({
   imports: [ConfigModule.forRoot(), UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService2,
+    {
+      provide: 'ABC',
+      useClass: AppService,
+    },
+    {
+      provide: 'Test',
+      useValue: ['test1', 'test2'],
+    },
+    {
+      provide: 'CCC',
+      inject: [AppService2],
+      async useFactory(AppService2: AppService2) {
+        console.log(AppService2.getHello())
+        return await new Promise((resolve) => resolve(AppService2.getHello()))
+      },
+    },
+  ],
 })
 export class AppModule {}
