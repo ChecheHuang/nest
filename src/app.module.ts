3ad670acae51d829da63b2a8c978d8ffec6b8ecf
Carl <qwerfcxzas1@gmail.com>
2024-02-12 17:14:35 +0800

    complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { DatabaseModule } from './database/database.module'
 import { UsersModule } from './users/users.module'
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [ConfigModule.forRoot(), UsersModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
