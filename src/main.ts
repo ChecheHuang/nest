46efbc1236555fee3ec59f447b3c20f10be46605
Carl <qwerfcxzas1@gmail.com>
2024-02-12 11:49:38 +0800

    setGlobalPrefix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.setGlobalPrefix('api')
+
   app.enableVersioning({
     type: VersioningType.URI,
   })
